1ac95de45ed538d91d9beba28681aa4a640eeecb
wangzuxiong <zuxiongwang@163.com>
2022-04-29 07:29:20 +0800

    clamp index

--- Assets/Scripts/WidgetSwitcher.cs
+++ Assets/Scripts/WidgetSwitcher.cs
@@ -4,10 +4,30 @@ using UnityEngine;
 using UnityEngine.UI;
 public class WidgetSwitcher : MonoBehaviour
 {
-    public int ActiveIndex = 0;
+    [SerializeField]
+    private int _activeIndex = 0;
+    public int ActiveIndex
+    {
+        get
+        {
+            return _activeIndex;
+        }
+        set
+        {
+            _activeIndex = value;
+            //_activeIndex = Mathf.Clamp(0, transform.childCount, value);
+        }
+    }
+
 
     private void Awake()
     {
+        SetActiveWidgetIndex(ActiveIndex);
+    }
+
+    public void SetActiveWidgetIndex(int activeIndex)
+    {
+        ActiveIndex = activeIndex;
         int length = transform.childCount;
 
         for (int i = 0; i < length; i++)
